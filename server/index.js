1278823240e5e35df853431c21e437e8573c8081
trybulem <marcin.trybulec@contractors.roche.com>
2016-09-15 17:22:28 +0200

    without

--- server/index.js
+++ server/index.js
@@ -1,10 +1,10 @@
 var express = require('express');
-var mongoose = require('mongoose');
+//var mongoose = require('mongoose');
 var path = require('path');
 
 // Connection URL
 var url = 'mongodb://localhost:27017/test';
-mongoose.connect(url);
+//mongoose.connect(url);
 
 var passport = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
@@ -31,50 +31,50 @@ app.use(express.static('client'));
 app.use(passport.initialize());
 app.use(passport.session());
 
-var passportLocalMongoose = require('passport-local-mongoose');
+// var passportLocalMongoose = require('passport-local-mongoose');
 
-var AccountSchema = new mongoose.Schema({
-    username: String,
-    password: String
-});
+// var AccountSchema = new mongoose.Schema({
+//     username: String,
+//     password: String
+// });
 
-AccountSchema.plugin(passportLocalMongoose);
-var Account = mongoose.model('Account', AccountSchema);
-passport.use(new LocalStrategy(Account.authenticate()));
-passport.serializeUser(Account.serializeUser());
-passport.deserializeUser(Account.deserializeUser());
+// AccountSchema.plugin(passportLocalMongoose);
+// var Account = mongoose.model('Account', AccountSchema);
+// passport.use(new LocalStrategy(Account.authenticate()));
+// passport.serializeUser(Account.serializeUser());
+// passport.deserializeUser(Account.deserializeUser());
 
 
 
 app.get('/', function (req, res) {
     res.sendFile(path.resolve('client/index.html'));
 });
-app.get('/login', function (req, res) {
-    res.sendFile(path.resolve('client/app/account/login.html'));
-});
-app.get('/register', function (req, res) {
-    res.sendFile(path.resolve('client/app/account/register.html'));
-});
+// app.get('/login', function (req, res) {
+//     res.sendFile(path.resolve('client/app/account/login.html'));
+// });
+// app.get('/register', function (req, res) {
+//     res.sendFile(path.resolve('client/app/account/register.html'));
+// });
 
-app.post('/register', function (req, res) {
-    Account.register(new Account({ username: req.body.username }), req.body.password, function (err, account) {
-        if (err) {
-            return res.sendFile(path.resolve('client/app/account/register.html'));
-        }
+// app.post('/register', function (req, res) {
+//     Account.register(new Account({ username: req.body.username }), req.body.password, function (err, account) {
+//         if (err) {
+//             return res.sendFile(path.resolve('client/app/account/register.html'));
+//         }
 
-        passport.authenticate('local')(req, res, function () {
-            res.redirect('/');
-        });
-    });
-});
-app.post('/login', passport.authenticate('local'), function (req, res) {
+//         passport.authenticate('local')(req, res, function () {
+//             res.redirect('/');
+//         });
+//     });
+// });
+// app.post('/login', passport.authenticate('local'), function (req, res) {
     
-    res.redirect('/');
-});
-app.get('/logout', function (req, res) {
-    req.logout();
-    res.redirect('/');
-});
+//     res.redirect('/');
+// });
+// app.get('/logout', function (req, res) {
+//     req.logout();
+//     res.redirect('/');
+// });
 app.listen(3000, function () {
     console.log("Server started at port 3003");
 });
