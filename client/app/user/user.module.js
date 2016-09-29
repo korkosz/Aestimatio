bf5383195540fdd7185ccfbf88fb2b76b5a472d0
korkosz <chmieltrybulec@gmail.com>
2016-09-29 12:53:54 +0200

    user grades

--- client/app/user/user.module.js
+++ client/app/user/user.module.js
@@ -2,7 +2,8 @@ var routes = require('./user.routes');
 
 module.exports = angular.module('aestimatio.user', [])
     .factory('userService', require('./user.service'))
-    .component('userGrades', require('./grades/grades.component'))
+    .component('userGrades', require('./grades/grades.component').grades)
+    .directive('gradeModal', require('./grades/grades.component').modal)
     .component('userAverageGrade', require('./average_grade/average_grade.component'))
     .config(routes);
 
--- client/app/user/user.routes.js
+++ client/app/user/user.routes.js
@@ -1,6 +1,11 @@
 module.exports = ['$routeProvider', function ($routeProvider) {
     $routeProvider
         .when('/user/grades', {
-            template: '<user-grades></user-grades>'           
+            template: '<user-grades user-class="$resolve.userClass"></user-grades>',
+            resolve: {
+                userClass: function(classService) {
+                    return classService.UserClass.$promise;
+                }
+            }           
         });
 }];
