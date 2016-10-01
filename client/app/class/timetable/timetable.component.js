0b41fdbc058485bdbafb3aba6c347d372a71202b
korkosz <chmieltrybulec@gmail.com>
2016-10-01 22:02:23 +0200

    timetable

--- client/app/class/timetable/timetable.component.js
+++ client/app/class/timetable/timetable.component.js
@@ -1,9 +1,22 @@
-class ctrl { }
-
-module.exports = {
+var component = {
     templateUrl: '/static/app/class/timetable/timetable.template.html',
-    controller: ctrl,
+    controller,
     bindings: {
         userClass: '='
     }
 };
+
+function controller() {
+    var vm = this;
+
+    vm.replaceSubject = function(dayIdx, originalSubjectIdx, newSubj) {
+        vm.userClass.timetable[dayIdx].subjects
+            .splice(originalSubjectIdx, 1, newSubj);
+
+        //close dropdown
+        $('.l-dropdown-wrapper.is-open')
+            .toggleClass('is-open is-close');
+    };    
+}
+
+module.exports = component;
