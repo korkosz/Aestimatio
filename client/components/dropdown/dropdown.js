8ad366387286e78fd7bdb6cbd0db8030fe06262b
korkosz <chmieltrybulec@gmail.com>
2016-10-06 01:41:39 +0200

    dropdown repaired

--- client/components/dropdown/dropdown.js
+++ client/components/dropdown/dropdown.js
@@ -5,10 +5,30 @@ app.directive('dropdown', function () {
         restrict: 'A',
         link(scope, el) {
             var btn = el.find('.l-dropdown__btn');
-            
-            btn.click(function () {
-                $(this).parent('.l-dropdown-wrapper')
-                    .toggleClass('is-open is-close');
+
+            btn.click(function (event) {
+                var btnClick = $(this);
+                var wrapper = btnClick.parent('.l-dropdown-wrapper');
+                var menu = btnClick.siblings('.l-dropdown__menu');
+
+                // open dropdown
+                event.stopPropagation(); // trigger only btn
+
+                wrapper.siblings()
+                    .removeClass('is-open')
+                    .addClass('is-close'); // make sure that every sibling is close
+
+                wrapper.removeClass('is-close')
+                    .addClass('is-open'); // open dropdown menu
+
+                // close dropdown
+                $(window).click( function (e) {
+                    if ( !menu.is(e.target)
+                         && menu.has(e.target).length === 0 ) {
+                            wrapper.removeClass('is-open').addClass('is-close');
+
+                    }
+                });
             });
         }
     };
