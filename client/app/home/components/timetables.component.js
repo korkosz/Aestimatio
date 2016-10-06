05dd47a701b6e9f855e4826ef301866df8b4fceb
korkosz <chmieltrybulec@gmail.com>
2016-10-07 00:36:08 +0200

    timetables

--- client/app/home/components/timetables.component.js
+++ client/app/home/components/timetables.component.js
@@ -5,6 +5,35 @@ module.exports = {
     bindings: {
         selectedDay: '<?'
     },
-    controller($scope) {
+    controller($scope, classService) {
+        var vm = this;
+
+        vm.$onInit = function () {
+            //classService.UserClass.timetable
+
+            $scope.$watch(() => vm.selectedDay, function () {
+                vm.currentDay = moment(vm.selectedDay);
+                vm.nextDay = vm.currentDay.clone().add(1, 'd');
+                vm.previousDay = vm.currentDay.clone().subtract(1, 'd');
+
+                vm.currentDayTT = classService.UserClass.timetable
+                    .find((day) => {
+                        return day.day.toLowerCase() ===
+                            vm.currentDay.format('dddd').toLowerCase();
+                    });
+                vm.nextDayTT = classService.UserClass.timetable
+                    .find((day) => {
+                        return day.day.toLowerCase() ===
+                            vm.nextDay.format('dddd').toLowerCase();
+                    });
+                vm.previousDayTT = classService.UserClass.timetable
+                    .find((day) => {
+                        return day.day.toLowerCase() ===
+                            vm.previousDay.format('dddd').toLowerCase();
+                    });
+            });
+        };
+
+
     }
 };
