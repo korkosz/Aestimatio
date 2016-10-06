2d66da99ad205156ca2fb4351dc3902e88052d18
korkosz <chmieltrybulec@gmail.com>
2016-10-06 23:50:49 +0200

    weekdays dziala

--- client/app/home/components/weekExams.component.js
+++ client/app/home/components/weekExams.component.js
@@ -10,25 +10,50 @@ module.exports = {
 
         vm.$onInit = function () {
             vm._selectedDay = moment(vm.selectedDay);
-
-            getExams(); 
+            vm.weekDays = [];
+            getExams();
+            combineWeekDaysAndExams();
 
             $scope.$watch(function () {
                 return classService.UserClass.tests.length;
             }, function () {
                 getExams();
+                combineWeekDaysAndExams();
             });
 
             $scope.$watch(() => vm.selectedDay, function () {
+                vm._selectedDay = moment(vm.selectedDay);
                 getExams();
+                combineWeekDaysAndExams();
             });
         };
 
+        function combineWeekDaysAndExams() {
+            const weekDaysNames = ['Monday', 'Tuesday',
+                'Wednesday', 'Thursday', 'Friday'];
+
+            vm.weekDays.length = 0;
+
+            for (let i = 0; i < 5; i++) {
+                vm.weekDays.push({
+                    name: weekDaysNames[i],
+                    exams: examsForDay(i + 1)
+                });
+            }
+        }
+
+        function examsForDay(dayIdx) {
+            //exams are already filtered for this week
+            return vm.exams.filter((ex) => {
+                return moment(ex.date).day() === dayIdx;
+            });
+        }
+
         function getExams() {
-            var startOfTheWeek = vm._selectedDay
+            var startOfTheWeek = moment(vm._selectedDay)
                 .startOf('week');
-            var endOfTheWeek = vm._selectedDay
-                .startOf('week');
+            var endOfTheWeek = moment(vm._selectedDay)
+                .endOf('week');
 
             vm.exams = classService.UserClass
                 .tests.filter((exam) => {
