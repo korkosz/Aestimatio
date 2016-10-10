21848c5dd1296379ff06a0dd3497b5100251d788
korkosz <chmieltrybulec@gmail.com>
2016-10-10 23:39:18 +0200

    search class

--- client/app/class/search/search.component.js
+++ client/app/class/search/search.component.js
@@ -1,6 +1,27 @@
 
 function controller($scope) {
-    this.huj = 1475672462722;
+    var vm = this;
+
+    vm.$onInit = function () {
+        vm.countries = [];
+        vm.cities = [];
+        vm.schools = [];
+        vm.classes = [];
+
+        vm.countrySearch = '';
+        vm.citySearch = '';
+        vm.schoolSearch = '';
+        vm.classSearch = '';
+
+        vm.country = null;
+        vm.city = null;
+        vm.school = null;
+        vm.class = null;
+    };
+
+    vm.handleCountrySearch = function (valid) {
+        console.log(valid);
+    };
 }
 
 module.exports = {
